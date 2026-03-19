0baa4601f7d1be4d8185455c735525edadd3c406
Stagiaire 7 <devsecpem@proton.me>
2026-03-19 09:38:04 +0100

    fix: suppr secrets et ext creds

--- src/config.php
+++ src/config.php
@@ -5,7 +5,7 @@
 $db_host = 'localhost';
 $db_name = 'meditrack_db';
 $db_user = 'root';
-$db_pass = 'M3d1Tr@ck_r00t_2024!';
+$db_pass = getenv('DB_PASSWORD');
 
 // Direct connection without error handling
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
